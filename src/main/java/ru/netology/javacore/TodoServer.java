af245a469b5f7d98417346be74d129dd27921cc7
evgeny <evgeniyvaganov@yandex.ru>
2022-06-17 21:23:41 +0500

    refactor

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,6 +1,9 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -8,14 +11,48 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
-        System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket socket = serverSocket.accept();
+             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            System.out.println("Starting server at " + port + "...");
+            JSONParser jsonParser = new JSONParser();
+
+            while (true) {
+                if (!bufferedReader.ready()) {
+                    break;
+                }
+                String json = bufferedReader.readLine();
+                Object o = jsonParser.parse(json);
+                JSONObject jsonObject = (JSONObject) o;
+                for (int i = 0; i < jsonObject.size(); ) {
+                    String typeTodos = (String) jsonObject.get("type");
+                    if (typeTodos.equals("ADD")) {
+                        String addTodos = (String) jsonObject.get("task");
+                        todos.addTask(addTodos);
+                    } else {
+                        String removeTodos = (String) jsonObject.get("task");
+                        todos.removeTask(removeTodos);
+                    }
+                }
+
+            }
+
+            String a = todos.getAllTasks();
+            System.out.println(a);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,33 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    List<String> taskList;
+    StringBuilder bd = new StringBuilder();
+
+    public Todos() {
+        taskList = new ArrayList<>();
+    }
 
     public void addTask(String task) {
-        //...
+        taskList.add(task);
     }
 
     public void removeTask(String task) {
-        //...
+        if (taskList.contains(task)) {
+            taskList.remove(task);
+        } else {
+            System.out.println("Задача отсутствует");
+        }
+
     }
 
     public String getAllTasks() {
-        //...
+        Collections.sort(taskList);
+
+        for (String c : taskList) {
+            bd.append(c + " ");
+        }
+        return bd.toString().trim();
     }
 
 }
