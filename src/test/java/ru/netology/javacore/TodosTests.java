60a5b991f51bc6d01ad6cb5aad11c930ed15baf2
evgeny <evgeniyvaganov@yandex.ru>
2022-06-28 20:08:42 +0500

    refactor tests

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -3,22 +3,19 @@ package ru.netology.javacore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodosTests {
     @Test
     void addTodosTest() {
         Todos todos = new Todos();
         String s = "Попить чай";
-        String s1 = "Выйти на пробежку";
-
-
         todos.addTask(s);
-        todos.addTask(s1);
 
-
-        String expected = "Выйти на пробежку Попить чай";
-        String result = todos.getAllTasks();
-
-
+        List<String> expected = new ArrayList<>();
+        expected.add(s);
+        List<String> result = todos.tasks;
         Assertions.assertEquals(expected, result);
     }
 
@@ -31,26 +28,23 @@ public class TodosTests {
         todos.addTask(s);
         todos.addTask(s1);
         todos.removeTask(s1);
-
-
-        String expected = "Попить чай";
-        String result = todos.getAllTasks();
-
-
+        List<String> expected = new ArrayList<>();
+        expected.add(s);
+        List<String> result = todos.tasks;
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    void removeTodosTestException() {
+    void getAllTasks() {
         Todos todos = new Todos();
         String s = "Попить чай";
         String s1 = "Выйти на пробежку";
-        String s2 = "Погулять";
 
         todos.addTask(s);
         todos.addTask(s1);
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> todos.removeTask(s2));
+        String result = todos.getAllTasks();
+        String expected = "Выйти на пробежку Попить чай ";
+        Assertions.assertEquals(expected, result);
     }
 
 }
